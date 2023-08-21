a23c268c202a7abf9a151c26ebdf66956506f424
Mintaek_CHO <mintaek5555@gmail.com>
2023-08-21 18:16:15 +0900

    git init

---
+++ src/components/ChartView.tsx
@@ -0,0 +1,62 @@
+
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "띠바",
+    },
+  },
+};
+
+const dataList_1 = [4, 20, 1, 3, 10, 13, 5];
+const dataList_2 = [5, 8, 2, 4, 12, 15, 7];
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "부엉이",
+      data: dataList_1.map((data) => data),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "올빼미",
+      data: dataList_2.map((data) => data),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export default function ChartView() {
+  return <Line options={options} data={data} />;
+}
